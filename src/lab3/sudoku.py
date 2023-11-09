395d107ee47a3887861d543dadf45c840ba5dd67
Roman <MyEducation11235@gmail.com>
2023-11-09 20:08:30 +0300

    add solve, check_solution, generate_sudoku

--- src/lab3/sudoku.py
+++ src/lab3/sudoku.py
@@ -1,6 +1,8 @@
 import pathlib
 import typing as tp
+import random
 
+alphabet = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}
 T = tp.TypeVar("T")
 
 
@@ -11,12 +13,10 @@ def read_sudoku(path: tp.Union[str, pathlib.Path]) -> tp.List[tp.List[str]]:
         puzzle = f.read()
     return create_grid(puzzle)
 
-
 def create_grid(puzzle: str) -> tp.List[tp.List[str]]:
     digits = [c for c in puzzle if c in "123456789."]
     grid = group(digits, 9)
     return grid
-
 
 def display(grid: tp.List[tp.List[str]]) -> None:
     """Вывод Судоку """
@@ -31,7 +31,6 @@ def display(grid: tp.List[tp.List[str]]) -> None:
         if str(row) in "25":
             print(line)
     print()
-
 
 def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
     """
@@ -49,7 +48,6 @@ def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
             res[i][j] = values[i*n + j]
     return res
 
-
 def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
     """Возвращает все значения для номера строки, указанной в pos
     >>> get_row([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']], (0, 0))
@@ -60,7 +58,6 @@ def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str
     ['.', '8', '9']
     """
     return grid[pos[0]].copy()
-
 
 def get_col(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
     """Возвращает все значения для номера столбца, указанного в pos
@@ -90,7 +87,6 @@ def get_block(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[s
     for i in range(3):
         res += grid[row + i][col:col+3]
     return res
-#get_block(read_sudoku('puzzle1.txt'), (8, 8))
 
 def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[int, int]]:
     """Найти первую свободную позицию в пазле
@@ -101,8 +97,11 @@ def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[in
     >>> find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
     (2, 0)
     """
-    pass
-
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] == '.':
+                return i, j
+    return -1, -1
 
 def find_possible_values(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.Set[str]:
     """Вернуть множество возможных значения для указанной позиции
@@ -114,8 +113,11 @@ def find_possible_values(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -
     >>> values == {'2', '5', '9'}
     True
     """
-    pass
-
+    act_row = set(get_row(grid, pos))
+    act_col = set(get_col(grid, pos))
+    act_block = set(get_block(grid, pos))
+    act_all = act_row | act_col | act_block
+    return alphabet - act_all
 
 def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     """ Решение пазла, заданного в grid """
@@ -129,14 +131,32 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    pass
-
+    pos = find_empty_positions(grid)
+    if pos[0] == -1:
+        return grid
+    for possible_values in find_possible_values(grid, pos):
+        grid[pos[0]][pos[1]] = possible_values
+        res = solve(grid)
+        if res != [[]]:
+            return res
+    grid[pos[0]][pos[1]] = '.'
+    return [[]]
 
 def check_solution(solution: tp.List[tp.List[str]]) -> bool:
-    """ Если решение solution верно, то вернуть True, в противном случае False """
-    # TODO: Add doctests with bad puzzles
-    pass
-
+    """ Если решение solution верно, то вернуть True, в противном случае False
+    >>> solution = [["5", "3", "4", "6", "7", "8", "9", "1", "2"],            ["6", "7", "2", "1", "9", "5", "3", "4", "8"],            ["1", "9", "8", "3", "4", "2", "5", "6", "7"],            ["8", "5", "9", "7", "6", "1", "4", "2", "3"],            ["4", "2", "6", "8", "5", "3", "7", "9", "1"],            ["7", "1", "3", "9", "2", "4", "8", "5", "6"],            ["9", "6", "1", "5", "3", "7", "2", "8", "4"],            ["2", "8", "7", "4", "1", "9", "6", "3", "5"],            ["3", "4", "5", "2", "8", "6", "1", "7", "9"],        ]
+    >>> check_solution(solution)
+    True
+    >>> solution = [["5", "5", "4", "6", "7", "8", "9", "1", "2"],            ["6", "7", "2", "1", "9", "5", "3", "4", "8"],            ["1", "9", "8", "3", "4", "2", "5", "6", "7"],            ["8", "5", "9", "7", "6", "1", "4", "2", "3"],            ["4", "2", "6", "8", "5", "3", "7", "9", "1"],            ["7", "1", "3", "9", "2", "4", "8", "5", "6"],            ["9", "6", "1", "5", "3", "7", "2", "8", "4"],            ["2", "8", "7", "4", "1", "9", "6", "3", "5"],            ["3", "4", "5", "2", "8", "6", "1", "7", "9"],        ]
+    >>> check_solution(solution)
+    False
+    """
+    for i in range(len(solution)):
+        if(set(get_row(solution, (i, 0))) != alphabet
+                or set(get_col(solution, (i, 0))) != alphabet
+                or set(get_block(solution, (i * 3 % 9, i // 3 * 3))) != alphabet):
+            return False
+    return True
 
 def generate_sudoku(N: int) -> tp.List[tp.List[str]]:
     """Генерация судоку заполненного на N элементов
@@ -159,8 +179,13 @@ def generate_sudoku(N: int) -> tp.List[tp.List[str]]:
     >>> check_solution(solution)
     True
     """
-    pass
-
+    N = min(N, 81)
+    res = [['.' for i in range(9)] for i in range(9)]
+    res = solve(res)
+    all_poses = [[i, j] for i in range(9) for j in range(9)]
+    for pos in random.sample(all_poses, 81 - N):
+        res[pos[0]][pos[1]] = '.'
+    return res
 
 if __name__ == "__main__":
     for fname in ["puzzle1.txt", "puzzle2.txt", "puzzle3.txt"]:
@@ -169,5 +194,7 @@ if __name__ == "__main__":
         solution = solve(grid)
         if not solution:
             print(f"Puzzle {fname} can't be solved")
+        elif check_solution(solution) == False:
+            print(f"Puzzle {fname} неправильно решён")
         else:
             display(solution)
